37f69059253bd2b4b7134f37255204564bd3f704
Sourav Malo <sourav.cse5.bu@gmail.com>
2021-01-24 17:24:37 +0600

    City and Region are shifted on location.php page

--- test.php
+++ test.php
@@ -1,16 +1,5 @@
 <?php
   $conn = mysqli_connect("localhost", "gamerea_cubeoutt", "gamerea_cubeout", "cubeoutmagi");
-  $sql = "SELECT * FROM scoreboard WHERE cityName = '';";
-  $result = mysqli_query($conn, $sql);
-  while($row = mysqli_fetch_assoc($result)) {
-    $id = $row["id"];
-    $ip = trim($row["ipAddress"]);
-
-    $ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
-
-    $city = $ipdat->geoplugin_city;
-
-    $sql_new = "UPDATE scoreboard SET cityName = '$city' WHERE id = $id;";
-    mysqli_query($conn, $sql_new);
-  }
+  
+  $sql_new = "UPDATE scoreboard SET cityName = '$city' WHERE id = $id;";
 ?>
